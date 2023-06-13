a9c5e088eae743b8b4dd4cb9b49f1186a584d5a5
Abed Nganga <ngangaabed@gmail.com>
2023-06-13 15:05:10 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+  function distanceFromHqInBlocks(blocks) {
+    if (blocks>=42) {
+      return  blocks - 42;
+    } else {
+      return 42 -blocks;
+    }
+  }
+
+  function distanceFromHqInFeet(blocks) {
+     return distanceFromHqInBlocks(blocks)*264;
+  }
+    
+  function distanceTravelledInFeet(start, destination) {
+    if (destination>=start) {
+      return  (destination - start) * 264;
+    } else {
+      return (start - destination) * 264;
+    }
+  }
+
+
+  
+  function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+    if (distanceInFeet <= 400) {
+      return 0; 
+
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+      return ((distanceInFeet - 400) * 0.02);
+
+    } else if (distanceInFeet > 2000 && distanceInFeet < 2500) {
+      return 25; 
+
+    } else if (distanceInFeet >= 2500) {
+      return 'cannot travel that far'; 
+    }
+  }
+  
